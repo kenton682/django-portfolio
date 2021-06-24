18f4c08fa17374a543f433f94363a8a13967ca1e
Kenton Olsen <kenton682@gmail.com>
2021-06-23 17:56:20 -0700

    added sortable by date feature in admin page

--- portfolio/base/admin.py
+++ portfolio/base/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin
 
 from .models import Card
 # Register your models here.
 
-class CardAdmin(admin.ModelAdmin):
-    fields=['pub_date', 'title', 'content', 'image']
+class CardAdmin(SortableAdminMixin, admin.ModelAdmin):
+    # fields=['pub_date', 'title', 'content', 'image']
+    pass
     
 admin.site.register(Card, CardAdmin)
--- portfolio/base/models.py
+++ portfolio/base/models.py
@@ -8,6 +8,13 @@ class Card(models.Model):
     content = models.CharField(max_length=250)
     image = models.ImageField(upload_to = "static/images", height_field= None, width_field= None, max_length= None)
     pub_date = models.DateTimeField('date published')
+    date_created = models.PositiveIntegerField(default=0, blank=False, null=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta(object):
+        ordering = ['date_created']
 
 
     
